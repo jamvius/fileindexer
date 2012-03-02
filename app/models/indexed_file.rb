8dda5f851a7a0632ba4e6d5c7ee91e22629fa8c8
jamvius <jamvius@gmail.com>
2012-03-02 14:45:26 +0100

    add scope for send deleted and duplicates

--- app/models/indexed_file.rb
+++ app/models/indexed_file.rb
@@ -4,4 +4,9 @@ class IndexedFile < ActiveRecord::Base
   def fullpath
     self.parent.fullpath
   end
+
+  #Method find duplicated files
+  scope :find_duplicates, select("md5, count(*) as total").group("md5").having("count(*) > 1").order("total")
+  #Method find delete files in device
+  scope :find_marked_as_deleted, lambda {|device_id| joins(:parent).where(:deleted => true, "indexed_directories.device_id" => device_id)}
 end
