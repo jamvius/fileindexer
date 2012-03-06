fcf362b1ff84a3f5f73eb3ae2d9668b8a5fffb61
jamvius <jamvius@gmail.com>
2012-03-06 18:05:27 +0100

    añadido find_root

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,10 @@
 class Device < ActiveRecord::Base
   has_many :indexer_tasks
+  has_many :indexed_directories
+
+  # @return [IndexedDirectory]
+  def find_root
+    IndexedDirectory.find_by_name_and_device_id(IndexedDirectory::ROOT_NAME, self.id)
+    #self.indexed_directories.select { |directory| directory.name == "/" }
+  end
 end
